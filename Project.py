b661487c1d31832be5b4f9555f7fd405e9a6983d
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-07 16:19:52 -0300

    Initialize Project.py with app configuration

---
+++ Project.py
@@ -0,0 +1,20 @@
+APP_TITLE = "Mobile-Porter"
+APP_FILE = "Main"
+PACKAGE = "com.brenninho.mobileporter"
+VERSION = "1.0.0"
+COMPANY = "Brenninho"
+
+ICON_PATH = "icon.png"
+
+ORIENTATION = "portrait"
+FULLSCREEN = True
+
+PERMISSIONS = [
+    "android.permission.WRITE_EXTERNAL_STORAGE",
+    "android.permission.READ_EXTERNAL_STORAGE",
+]
+
+ASSETS_DIR = "assets"
+SOURCE_DIR = "source"
+
+SUPPORTED_PLATFORMS = ["android", "ios"]
